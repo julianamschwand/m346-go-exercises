d18b607763439f7dd58869530fe2028b34b3c5c7
julianamschwand <julian_amschwand@sluz.ch>
2024-12-06 15:25:23 +0100

    Aufgaben Block 1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+var firstName, lastName = "Julian", "Amschwand"
+var dayOfBirth, monthOfBirth, yearOfBirth = 12, 06, 2008
+var numberOfSiblings = 2
+var heightInMeters = 1.67
+var zodiacSign = '\u264A'
+
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
